c921d74f138f0f3cc10dccba6e492f80ba99f1c6
kavacon <kavacon@icloud.com>
2023-04-02 11:55:42 +1000

    move resources into dist

--- src/util.ts
+++ src/util.ts
@@ -5,5 +5,5 @@ export const enum Resource {
 }
 
 export function getResourcePath(resource: Resource): string {
-    return path.join(__dirname, resource);
+    return path.join(__dirname, 'resource', resource);
 }
